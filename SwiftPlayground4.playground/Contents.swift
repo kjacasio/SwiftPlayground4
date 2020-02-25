7760b624769469ba32e5a82c31996f8d08cd7792
kat <kjacasio@hawaii.edu>
2020-02-25 10:15:39 -1000

    Swiftplaygrounds#4: Intro to Loops Feb 25 complete parts 1-6 & problem sets

--- SwiftPlayground4.playground/Contents.swift
+++ SwiftPlayground4.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//part 4
+// Using the loops to make the numbers 1-5
+for number in 1...5{
+    print(number)
+}
+
+//part 5
+//display the side panel view, has made a variable to make it equal to "sum"
+var sum = 0
+for number in 1...5{
+    sum += number
+    print(sum)
+}
+
+//part 6 while loops
+// introduce while loop in order to make a conditional statement to make it false
+var i = 1
+while i <= 10
+{
+    print(i)
+    i = i + 1
+}
+//in order to make the printing work, "i" needs to be a variable
